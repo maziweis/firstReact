7b51097b09f01ce63e6f88748998fa11bdb90bec
maziweis <201512895@qq.com>
2019-07-17 17:17:52 +0800

    121

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Api'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
